a35845679edd7e702167db11f13f133681bf8376
mosken.fatec <matheus.diegues@fatec.sp.gov.br>
2023-09-15 22:33:41 -0300

    Versão 1

---
+++ atividadeC/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 10;
+    float b = 11;
+    char c = 'aku';
+    double d = 10.33;
+
+
+
+
+
+    printf("Hello world!\n");
+    printf("%d \n",a);
+    printf("%.f \n",b);
+    printf("%c \n",c);
+
+
+
+
+    printf("%.f", a + b);
+
+
+    return 0;
+}
